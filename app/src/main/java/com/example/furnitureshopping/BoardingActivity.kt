c872db256c37d495578c4962b41cd53dff624e51
Ömer Okumuş <omerokumus3@gmail.com>
2025-01-24 22:06:43 +0300

    BoardingActivity to LoginActivity

--- app/src/main/java/com/example/furnitureshopping/BoardingActivity.kt
+++ app/src/main/java/com/example/furnitureshopping/BoardingActivity.kt
@@ -1,20 +1,31 @@
 package com.example.furnitureshopping
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.furnitureshopping.databinding.ActivityBoardingBinding
 
 class BoardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_boarding)
+        val binding = ActivityBoardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.getStartedButton.setOnClickListener {
+            Intent(this, LoginActivity::class.java).also {
+                startActivity(it)
+                finish()
+            }
+        }
+
     }
 }
