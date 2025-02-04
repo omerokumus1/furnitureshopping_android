8bfc1c156a1ebfe80215db9be76724d0f07dfc98
Ömer Okumuş <omerokumus3@gmail.com>
2025-02-04 10:35:24 +0300

    Refactor: Move BoardingActivity to feature/boarding package

--- app/src/main/java/com/example/furnitureshopping/feature/boarding/BoardingActivity.kt
+++ app/src/main/java/com/example/furnitureshopping/feature/boarding/BoardingActivity.kt
@@ -1,4 +1,4 @@
-package com.example.furnitureshopping
+package com.example.furnitureshopping.feature.boarding
 
 import android.content.Intent
 import android.os.Bundle
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.furnitureshopping.R
 import com.example.furnitureshopping.databinding.ActivityBoardingBinding
 import com.example.furnitureshopping.feature.auth.AuthActivity
 
