078433cbca8fe5250b50f072418aa856ec0f8e00
Ömer Okumuş <omerokumus3@gmail.com>
2025-03-15 14:39:04 +0300

    Constants, image_loading.png, image_not_available.png

---
+++ app/src/main/java/com/omerokumus/furnitureshopping/base/constants/Constants.kt
@@ -0,0 +1,6 @@
+package com.omerokumus.furnitureshopping.base.constants
+
+object Constants {
+    const val BASE_URL = "https://furniture-shopping-spring.onrender.com/api/"
+    const val OK_HTTP_DEFAULT_TIMEOUT: Long = 60
+}
