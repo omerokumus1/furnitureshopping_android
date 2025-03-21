068197a1a57db339a0c1520989723ff3f24d19b6
eminokumus1 <eminokumus1@gmail.com>
2025-03-21 17:29:09 +0300

    Glide used for displaying product images

--- app/src/main/java/com/omerokumus/furnitureshopping/feature/productdetail/presentation/ProductImgPagerAdapter.kt
+++ app/src/main/java/com/omerokumus/furnitureshopping/feature/productdetail/presentation/ProductImgPagerAdapter.kt
@@ -3,9 +3,12 @@ package com.omerokumus.furnitureshopping.feature.productdetail.presentation
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.omerokumus.furnitureshopping.R
+import com.omerokumus.furnitureshopping.base.constants.Constants
 import com.omerokumus.furnitureshopping.databinding.ProductImgLayoutBinding
 
-class ProductImgPagerAdapter(var productImgList: List<Int>) :
+class ProductImgPagerAdapter(var productImgList: List<String>) :
     RecyclerView.Adapter<ProductImgPagerAdapter.ProductImgViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductImgViewHolder {
         val binding =
@@ -14,7 +17,12 @@ class ProductImgPagerAdapter(var productImgList: List<Int>) :
     }
 
     override fun onBindViewHolder(holder: ProductImgViewHolder, position: Int) {
-        holder.binding.productImg.setImageResource(productImgList[position])
+        Glide.with(holder.itemView.context)
+            .load(Constants.BASE_URL + "products/images/" + productImgList[position])
+            .placeholder(R.drawable.image_loading)
+            .error(R.drawable.image_not_available)
+            .into(holder.binding.productImg)
+
     }
 
     override fun getItemCount(): Int {
