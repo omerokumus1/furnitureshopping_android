eb338455b702326242e6c43e3f792cae988ff84f
eminokumus1 <eminokumus1@gmail.com>
2025-03-24 20:56:45 +0300

    addUser service problem solved

--- app/src/main/java/com/omerokumus/furnitureshopping/feature/auth/UserApi.kt
+++ app/src/main/java/com/omerokumus/furnitureshopping/feature/auth/UserApi.kt
@@ -1,5 +1,6 @@
 package com.omerokumus.furnitureshopping.feature.auth
 
+import com.omerokumus.furnitureshopping.feature.auth.signup.data.model.UserRequest
 import com.omerokumus.furnitureshopping.feature.usermanager.model.User
 import retrofit2.Response
 import retrofit2.http.GET
@@ -9,7 +10,7 @@ import retrofit2.http.Path
 interface UserApi {
 
     @POST("/api/users/add-user")
-    suspend fun addUser(@Path("fullName") fullName: String, @Path("email") email: String)
+    suspend fun addUser(userRequest: UserRequest)
 
     @GET("/api/users/{userId}")
     suspend fun getUserById(@Path("userId") userId: Int): Response<User>
---
+++ app/src/main/java/com/omerokumus/furnitureshopping/feature/auth/signup/data/model/UserRequest.kt
@@ -0,0 +1,6 @@
+package com.omerokumus.furnitureshopping.feature.auth.signup.data.model
+
+data class UserRequest(
+    val fullName: String,
+    val email: String,
+)
